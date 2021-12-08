4173148ebbcf9f8c7e9e6bc9d9235950fe62730a
Mafzaidi <maftuhzaidi93@gmail.com>
2021-12-09 02:22:28 +0700

    create navbar and sidebar v_home.php, add main.css

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -9,26 +9,53 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <title>Welcome to CodeIgniter</title>
     <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap-4.6.1/dist/css/bootstrap.min.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/home.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/main.css'); ?>">
 </head>
 
 <body>
-    <!-- navbar -->
-    <nav class="navbar navbar-expand-sm navbar-light bg-light">
-        <a href="#" class="navbar-brand fa fa-e">ndemik</a>
-        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarMenu">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Products</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Users</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+<div id="wrapper">
+    <!-- Sidebar -->
+    <ul class="nav flex-column navbar-light bg-light sidebar" id="sidebar">
+        <a href="#" class="d-flex sidebar-brand align-items-center justify-content-center fa fa-e">ndemik</a>
+        <hr class="sidebar-divider my-0">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Form</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+        </li>
+    </ul>
+    <!-- End of Sidebar -->
+
+    <!-- Main Content -->
+    <div id="content-wrapper" class="d-flex flex-column">
+        <!-- navbar -->
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarMenu">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Users</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- End of Navbar -->
+    </div>
+    <!-- End of Main Content -->
+
+</div>
 
     <div class="container-fluid">
 
